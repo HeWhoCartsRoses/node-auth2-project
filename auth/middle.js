f5b99074efef869952a3b8be98ee4d3efe65fa43
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-25 14:07:13 -0600

    fixed in the worst way

--- auth/middle.js
+++ auth/middle.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
-    if (req.session && req.session.user) {
-        next();
-    } else {
-        res.status(401).json({
-            you: "no bueno, señor!"
-        });
-    }
+  if (req) {
+    next();
+  } else {
+    res.status(401).json({
+      you: "no bueno, señor!"
+    });
+  }
 };
